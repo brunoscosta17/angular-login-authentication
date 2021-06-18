2c86fcdc05a297ceba23c0ca590681068d0fcd32
= <=>
2021-06-18 11:31:56 -0300

    header component

--- frontend/src/app/modules/public/public.module.ts
+++ frontend/src/app/modules/public/public.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { PublicRoutingModule } from "./public-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicRoutingModule } from './public-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // containers
-import { PublicComponent } from "./public.component";
+import { PublicComponent } from './public.component';
+
+// components
+import { HeaderComponent } from './home/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 // components
 
 @NgModule({
     declarations: [
         // containers
-        PublicComponent
+        PublicComponent,
+        // components
+        HomeComponent,
+        HeaderComponent
     ],
     imports: [
         CommonModule,
