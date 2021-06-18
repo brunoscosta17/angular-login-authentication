964ed1e51bd7e8634fee01cec08d414574f229fa
= <=>
2021-06-18 03:18:15 -0300

    home dashboard

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { faTachometerAlt, faCalendarAlt, faBook, faCog, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faTachometerAlt, faCalendarAlt, faBook, faCog, faAngleDown, faAlignLeft, faHome, faIdCard, faToolbox, faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,15 +9,27 @@ import { faTachometerAlt, faCalendarAlt, faBook, faCog, faAngleDown } from '@for
 })
 export class SidebarComponent implements OnInit {
 
+  status = true;
+
   faTachometerAlt = faTachometerAlt;
   faCalendarAlt = faCalendarAlt;
   faBook = faBook;
   faCog = faCog;
   faAngleDown = faAngleDown;
+  faAlignLeft = faAlignLeft;
+  faHome = faHome;
+  faIdCard = faIdCard;
+  faToolbox = faToolbox;
+  faPhoneVolume = faPhoneVolume;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line: typedef
+  toggle() {
+    this.status = !this.status;
   }
 
 }
